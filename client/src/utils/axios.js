5b571742cab0c05473cb7ee5190a745405951fa2
Arpit Sharma <arpitsharma1004@gmail.com>
2026-08-08 02:29:27 +0530

    Update API configuration for deployment

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,32 +1,18 @@
 import axios from "axios";
 
-
 const API = axios.create({
-
-baseURL:"http://localhost:5000/api"
-
+    baseURL: "https://textile-marketplace-8nga.onrender.com/api"
 });
 
+API.interceptors.request.use((config) => {
 
-API.interceptors.request.use((config)=>{
+    const token = localStorage.getItem("token");
 
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-const token =
-localStorage.getItem("token");
-
-
-if(token){
-
-config.headers.Authorization =
-`Bearer ${token}`;
-
-}
-
-
-return config;
-
-
+    return config;
 });
-
 
 export default API;
