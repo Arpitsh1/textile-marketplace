779c65b7464540d9f71ae7ae43195f6226553cdf
Arpit Sharma <arpitsharma1004@gmail.com>
2026-08-08 11:13:50 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -19,18 +19,17 @@ connectDB();
 // Create Express app
 const app = express();
 
-
 // Middleware
 app.use(cors({
-    origin:[
-        "http://localhost:5173",
-        "https://textile-marketplace-ogzbr5bk6-arpitsh1s-projects.vercel.app"
-    ],
-    credentials:true
+  origin: [
+    "http://localhost:5173",
+    "https://textile-marketplace-omega.vercel.app",
+    "https://textile-marketplace-ogzbr5bk6-arpitsh1s-projects.vercel.app"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
-
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -47,18 +46,16 @@ app.use("/api/supplier", supplierRoutes);
 
 app.use("/api/reviews", require("./routes/reviewRoutes"));
 
-
 // Health Route
-app.get("/api/health", (req,res)=>{
-    res.json({
-        message:"Server is running"
-    });
+app.get("/api/health", (req, res) => {
+  res.json({
+    message: "Server is running"
+  });
 });
-
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
